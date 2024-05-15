8add01408b2d73ab9392b70b1dedd208e44ddbe5
Talha Toğuşlu <63349865+TalhaToguslu@users.noreply.github.com>
2024-05-15 09:28:43 +0300

    Add files via upload

---
+++ Infrastructure/SurveyAppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Model;
+using Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Npgsql;
+
+namespace Infrastructure;
+
+public class SurveyAppDbContext : DbContext
+{
+    public DbSet<Survey> Surveys { get; set; }
+    public DbSet<Option> Options { get; set; }
+    public DbSet<Vote> Votes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = new NpgsqlDataSourceBuilder("Server=localhost;Port=5432;Database=survey;Userid=postgres;Password=123456;Include Error Detail=True;");
+        builder.EnableDynamicJson(); 
+        var dataSource = builder.Build();
+        optionsBuilder.UseNpgsql(dataSource);
+        base.OnConfiguring(optionsBuilder);
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SurveyConfiguration).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
